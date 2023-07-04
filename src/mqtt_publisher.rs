b9143862e40db80b9fa25eb99dbb96adced0dc5e
Craftor <craftor@126.com>
2023-07-04 08:54:28 +0800

    init

---
+++ src/mqtt_publisher.rs
@@ -0,0 +1,106 @@
+use log::{debug, error, info, warn};
+use std::sync::Arc;
+use std::thread;
+use std::{process, time::Duration};
+
+extern crate paho_mqtt as mqtt;
+
+pub struct MqttClient<'a> {
+    client: Arc<mqtt::Client>,
+    host: &'a str,
+    client_id: &'a str,
+    username: &'a str,
+    password: &'a str,
+    clear: bool,
+}
+
+impl<'a> MqttClient<'a> {
+    /// init client using default configuration
+    pub fn new(
+        host: &'a str,
+        client_id: &'a str,
+        username: &'a str,
+        password: &'a str,
+        clear: bool,
+    ) -> Self {
+        // Define the set of options for the create.
+        // Use an ID for a persistent session.
+        let create_opts = mqtt::CreateOptionsBuilder::new()
+            .server_uri(host)
+            .client_id(client_id)
+            .finalize();
+
+        // Create a client.
+        let client = Arc::new(mqtt::Client::new(create_opts).unwrap_or_else(|err| {
+            error!("Error creating the client: {:?}", err);
+            process::exit(1);
+        }));
+
+        MqttClient {
+            client,
+            host,
+            client_id,
+            username,
+            password,
+            clear,
+        }
+    }
+
+    /// connect to the server
+    pub fn connect(&mut self) -> bool {
+        // Define the set of options for the connection.
+        let conn_opts = mqtt::ConnectOptionsBuilder::new()
+            .keep_alive_interval(Duration::from_secs(20))
+            .user_name(self.username)
+            .password(self.password)
+            .clean_session(self.clear)
+            .finalize();
+
+        // Connect and wait for it to complete or fail.
+        if let Err(e) = self.client.connect(conn_opts) {
+            error!("Unable to connect:\n\t{:?}", e);
+            false
+        } else {
+            info!("Connected to [{}] as [{}]", self.host, self.client_id);
+            true
+        }
+    }
+
+    // Reconnect to the broker when connection is lost.
+    pub fn try_reconnect(&mut self) -> bool {
+        warn!("Connection lost. Waiting to retry connection");
+        for _ in 0..5 {
+            thread::sleep(Duration::from_millis(5000));
+            if self.client.reconnect().is_ok() {
+                info!("Successfully reconnected");
+                return true;
+            }
+        }
+        error!("Unable to reconnect after several attempts.");
+        false
+    }
+
+    /// publis a message to the server
+    pub fn publish(&mut self, topic: String, msg: String, qos: i32) -> Result<(), mqtt::Error> {
+        debug!("Publishing messages on the {:?} topic", topic);
+        let msg = mqtt::Message::new(topic, msg.clone(), qos);
+        let tok = self.client.publish(msg);
+        match tok {
+            Err(e) => {
+                error!("Error sending message: {:?}", e);
+                Err(e)
+            }
+            Ok(_) => Ok(()),
+        }
+    }
+
+    // disconnect
+    pub fn disconnect(&mut self) {
+        // If still connected, then disconnect now.
+        if self.client.is_connected() {
+            println!("Disconnecting");
+            self.client.disconnect(None).unwrap();
+        }
+        println!("Exiting");
+    }
+}
---
+++ src/mqtt_subsriber.rs
@@ -0,0 +1,134 @@
+extern crate log;
+extern crate pretty_env_logger;
+
+use log::{error, info, warn};
+use mqtt::Message;
+use std::{sync::Arc, thread, time::Duration};
+use taos::Taos;
+
+extern crate paho_mqtt as mqtt;
+
+pub struct MqttSubsriber<'a> {
+    client: Arc<mqtt::Client>,
+    host: &'a str,
+    client_id: &'a str,
+    topic: &'a str,
+    qos: i32,
+    username: &'a str,
+    password: &'a str,
+}
+
+impl<'a> MqttSubsriber<'a> {
+    /// new mqtt server
+    pub fn new(
+        host: &'a str,
+        client_id: &'a str,
+        username: &'a str,
+        password: &'a str,
+        topic: &'a str,
+        qos: i32,
+    ) -> Self {
+        // create options
+        let create_opts = mqtt::CreateOptionsBuilder::new()
+            .server_uri(host)
+            .client_id(client_id)
+            .finalize();
+
+        // Create a client.
+        let cli = Arc::new(mqtt::Client::new(create_opts).unwrap());
+
+        MqttSubsriber {
+            client: cli,
+            host,
+            client_id,
+            topic,
+            qos,
+            username,
+            password,
+        }
+    }
+
+    // connect to server
+    pub fn connect(&mut self) -> bool {
+        // Define the set of options for the connection.
+        let lwt = mqtt::MessageBuilder::new()
+            .topic("test")
+            .payload("Consumer lost connection")
+            .finalize();
+
+        let conn_opts = mqtt::ConnectOptionsBuilder::new()
+            .keep_alive_interval(Duration::from_secs(20))
+            .user_name(self.username.clone())
+            .password(self.password.clone())
+            .clean_session(false)
+            .will_message(lwt)
+            .finalize();
+
+        // Connect and wait for it to complete or fail.
+        if let Err(e) = self.client.connect(conn_opts) {
+            error!("Unable to connect:\n\t{:?}", e);
+            false
+        } else {
+            info!("Connected to [{}] as [{}]", self.host, self.client_id);
+            true
+        }
+    }
+
+    // Subscribes single topic
+    pub fn subscribe_topic(&mut self) {
+        info!("Subscribing topic [{:#?}]", self.topic);
+        if let Err(e) = self.client.subscribe(self.topic, self.qos) {
+            error!("Error subscribes topics: {:?}", e);
+        } else {
+        }
+    }
+
+    // Reconnect to the broker when connection is lost.
+    pub fn try_reconnect(&mut self) -> bool {
+        warn!("Connection lost. Waiting to retry connection");
+        for _ in 0..12 {
+            thread::sleep(Duration::from_millis(5000));
+            if self.client.reconnect().is_ok() {
+                info!("Successfully reconnected");
+                return true;
+            }
+        }
+        error!("Unable to reconnect after several attempts.");
+        false
+    }
+
+    // processing rx incoming messages
+    pub async fn start<F>(&mut self, taos: &Taos, process_fn: F)
+    where
+        F: Fn(Message, &Taos) + Send + 'static,
+    {
+        let rx = self.client.start_consuming();
+
+        // let taos = init_tdengine_adxl(database_url, db_name).await.unwrap();
+
+        info!("Processing requests...");
+        for msg in rx.iter() {
+            if let Some(msg) = msg {
+                process_fn(msg, &taos);
+            } else if !self.client.is_connected() {
+                if self.try_reconnect() {
+                    info!("Resubscribe topics...");
+                    self.subscribe_topic();
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+
+    // disconnect
+    pub fn disconnect(&mut self) {
+        // If still connected, then disconnect now.
+        if self.client.is_connected() {
+            println!("Disconnecting");
+            self.client.unsubscribe(self.topic).unwrap();
+            self.client.disconnect(None).unwrap();
+        }
+        println!("Exiting");
+    }
+}
